76e5770fe7924950832f9ca2d5debbe1daab10e1
Kseylo <alekseysarychev29@gmail.com>
2021-12-05 14:05:51 +0700

    updating gulp and some css changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,39 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
-function style () {
-    return gulp.src('./scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream())
+function style() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 }
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    })
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch("./scss/**/*.scss", style);
+  gulp.watch("./*.html").on("change", browserSync.reload);
 }
 
 function prefix() {
-    return gulp.src('./css/style.css')
+  return gulp
+    .src("./css/style.css")
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest("./css/"));
 }
 
 function clean() {
-    return gulp.src("./style.css")
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest("./"))
+  return gulp
+    .src("./css/style.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("./css/"));
 }
 
 exports.clean = clean;
