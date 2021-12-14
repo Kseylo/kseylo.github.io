c216b7e98e7db6ee9462ecbe89c6834f6db05faa
Kseylo <alekseysarychev29@gmail.com>
2021-12-15 06:18:07 +0700

    fixes

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kseylo:190498@localhost/cherry'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    size_small = db.Column(db.Integer)
+    price_small = db.Column(db.Integer)
+    size_big = db.Column(db.Integer)
+    price_big = db.Column(db.Integer)
+
+# db.create_all()
 
 @app.route("/")
 def home():
@@ -16,7 +31,8 @@ def locations():
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html")
+    menu = Menu.query.all()
+    return render_template("menu.html", menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
